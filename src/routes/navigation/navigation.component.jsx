3ef1ccf1fe4237c63e132af3ee462be1266562ac
ccksam <chanyuksingsam@gmail.com>
2023-03-16 21:33:49 +0800

    [IMP] v1.0.0 - routing pages using RRD | add svg logo | create <Navigation> for page links
    all <Routes> registerd in App.js | use <Outlet> to render index route component | add <a> & box-sizing css

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,24 @@
+import { Outlet, Link } from "react-router-dom";
+import { ReactComponent as Logo } from "../../assets/crown.svg";
+import "./navigation.styles.scss";
+
+const Navigation = () => (
+	<>
+		<div className="navigation">
+			<Link to="/" className="logo-container">
+				<Logo className="logo" />
+			</Link>
+			<div className="nav-links-container">
+				<Link className="nav-link" to="/shop">
+					Shop
+				</Link>
+				<Link className="nav-link">Contact</Link>
+				<Link className="nav-link">Sign In</Link>
+				<Link className="nav-link">Cart</Link>
+			</div>
+		</div>
+		<Outlet />
+	</>
+);
+
+export default Navigation;
