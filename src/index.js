49d884aa9c205eb78e9d0db96cacf3c67cb54364
ccksam <chanyuksingsam@gmail.com>
2023-05-11 04:56:14 +0800

    [IMP] v0.0.0 - add redux/toolkit | adjust basic config of redux store
    comment out unneccessary deprecated config

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
+// import { PersistGate } from "redux-persist/integration/react";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { store, persistor } from "./store/store";
@@ -11,11 +11,11 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
 		<Provider store={store}>
-			<PersistGate persistor={persistor} loading={null}>
-				<BrowserRouter>
-					<App />
-				</BrowserRouter>
-			</PersistGate>
+			{/* <PersistGate persistor={persistor} loading={null}> */}
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
+			{/* </PersistGate> */}
 		</Provider>
 	</React.StrictMode>
 );
--- src/store/store.js
+++ src/store/store.js
@@ -1,43 +1,47 @@
 import { compose, createStore, applyMiddleware } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
+
 import logger from "redux-logger";
-// import myLogger from "./middleware/myLogger";
-import { persistReducer, persistStore } from "redux-persist";
-import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
+// import { persistReducer, persistStore } from "redux-persist";
+// import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
 import rootReducer from "./root-reducer";
 
-// "cart" redux reducer value will store at browser's localStorage
-//		cart state value will stay (rehydrated) after refresh, so UI render based on previous session
-// *note*
-// 		1. "user", "categories" blacklist because already store at firebase cloud storage, avoid unexpected conflict
-// 		2. The main features of localStorage are: Shared between all tabs and windows from the same origin. The data does not expire. It remains after the browser restart and even OS reboot.
-const persistConfig = {
-	key: "root",
-	storage,
-	blacklist: ["user", "categories"], // user reducer will not be persist
-};
+// // "cart" redux reducer value will store at browser's localStorage
+// //		cart state value will stay (rehydrated) after refresh, so UI render based on previous session
+// // *note*
+// // 		1. "user", "categories" blacklist because already store at firebase cloud storage, avoid unexpected conflict
+// // 		2. The main features of localStorage are: Shared between all tabs and windows from the same origin. The data does not expire. It remains after the browser restart and even OS reboot.
+// const persistConfig = {
+// 	key: "root",
+// 	storage,
+// 	blacklist: ["user", "categories"], // user reducer will not be persist
+// };
 
-const persistedReducer = persistReducer(persistConfig, rootReducer);
+// const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-// create a middleware chain array to be applied
-// *note*
-// 		in "production" mode, we won't console.log anything
-// 			so return [logger] only when in "development" or other testing enviroment
+// // create a middleware chain array to be applied
+// // *note*
+// // 		in "production" mode, we won't console.log anything
+// // 			so return [logger] only when in "development" or other testing enviroment
 const middleWares = [process.env.NODE_ENV !== "production" && logger].filter(Boolean);
-// const middleWares = [process.env.NODE_ENV === "development" && myLogger].filter(Boolean);
+// // const middleWares = [process.env.NODE_ENV === "development" && myLogger].filter(Boolean);
 
-// when not in production env && in browser && redux devtools extension installed,
-// 		then use the redux devtools compose so that enable redux extension in browser;
-// 			otherwise use redux compose
-const composeEnhancer =
-	(process.env.NODE_ENV !== "production" &&
-		window &&
-		window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
-	compose;
+// // when not in production env && in browser && redux devtools extension installed,
+// // 		then use the redux devtools compose so that enable redux extension in browser;
+// // 			otherwise use redux compose
+// const composeEnhancer =
+// 	(process.env.NODE_ENV !== "production" &&
+// 		window &&
+// 		window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+// 	compose;
 
-// compose a enhancers chain, (middleware is one of the enhancer)
-const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
+// // compose a enhancers chain, (middleware is one of the enhancer)
+// const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
 
-// return a single Redux store that lets you read the state, dispatch actions, and subscribe to changes.
-export const store = createStore(persistedReducer, undefined, composedEnhancers);
+// // return a single Redux store that lets you read the state, dispatch actions, and subscribe to changes.
+export const store = configureStore({
+	reducer: rootReducer,
+	// middleware: middleWares,
+});
 
-export const persistor = persistStore(store);
+// export const persistor = persistStore(store);
