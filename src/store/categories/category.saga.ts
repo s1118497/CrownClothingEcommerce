38f5c23cd1a3c5c39c1828880bb6363f482ef4e5
ccksam <chanyuksingsam@gmail.com>
2023-06-15 21:46:19 +0800

    [ADD] v13.4.0 - install "typed-redux-saga" | typing category saga

--- src/store/categories/category.saga.ts
+++ src/store/categories/category.saga.ts
@@ -1,22 +1,22 @@
-import { takeLatest, all, call, put } from "redux-saga/effects";
+import { takeLatest, all, call, put } from "typed-redux-saga";
 import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
 import { fetchCategoriesSuccess, fetchCategoriesFail } from "./category.action";
 import { CATEGORIES_ACTION_TYPES } from "./category.types";
 
 export function* fetchCategoriesAsync() {
 	try {
-		const categoriesArray = yield call(getCategoriesAndDocuments);
-		yield put(fetchCategoriesSuccess(categoriesArray));
+		const categoriesArray = yield* call(getCategoriesAndDocuments);
+		yield* put(fetchCategoriesSuccess(categoriesArray));
 	} catch (err) {
-		yield put(fetchCategoriesFail(err));
+		yield* put(fetchCategoriesFail(err as Error));
 	}
 }
 
 export function* onFetchCategories() {
-	yield takeLatest(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START, fetchCategoriesAsync);
+	yield* takeLatest(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START, fetchCategoriesAsync);
 }
 
 // aggregator of all sagas related to category
 export function* categoriesSaga() {
-	yield all([call(onFetchCategories)]);
+	yield* all([call(onFetchCategories)]);
 }
