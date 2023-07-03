1e006e0c00b6bae6ed86798014667d5574da0b37
ccksam <chanyuksingsam@gmail.com>
2023-07-03 12:58:33 +0800

    [IMP] v14.3.3 - mirgrate all route components to lazy loading | <Spinner> as Suspense fallback

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-import { useEffect } from "react";
+import { useEffect, lazy, Suspense } from "react";
+
 import { Routes, Route } from "react-router-dom";
 
 import { useDispatch } from "react-redux";
 
 import { checkUserSession } from "./store/user/user.action";
 
-import Home from "./routes/home/home.component";
-import Navigation from "./routes/navigation/navigation.component";
-import Authentication from "./routes/authentication/authentication.component";
-import Shop from "./routes/shop/shop.component";
-import Checkout from "./routes/checkout/checkout.component";
+import Spinner from "./components/spinner/spinner.component";
+
+// Static Import
+// import Home from "./routes/home/home.component";
+// import Authentication from "./routes/authentication/authentication.component";
+// import Shop from "./routes/shop/shop.component";
+// import Checkout from "./routes/checkout/checkout.component";
+// import Navigation from "./routes/navigation/navigation.component";
+
+// Dynamic Import, lazy load components
+const Home = lazy(() => import("./routes/home/home.component"));
+const Navigation = lazy(() => import("./routes/navigation/navigation.component"));
+const Authentication = lazy(() => import("./routes/authentication/authentication.component"));
+const Shop = lazy(() => import("./routes/shop/shop.component"));
+const Checkout = lazy(() => import("./routes/checkout/checkout.component"));
 
 const App = () => {
 	const dispatch = useDispatch();
@@ -19,17 +30,19 @@ const App = () => {
 	}, []);
 
 	return (
-		<Routes>
-			{/* <Navigation> will always render in every path */}
-			<Route path="/" element={<Navigation />}>
-				{/* <Home> will render when root route, at <Navigation>'s <Outlet> */}
-				<Route index element={<Home />} />
-				{/*  	match any route that prefix "/shop/"	*/}
-				<Route path="shop/*" element={<Shop />} />
-				<Route path="auth" element={<Authentication />} />
-				<Route path="checkout" element={<Checkout />} />
-			</Route>
-		</Routes>
+		<Suspense fallback={<Spinner />}>
+			<Routes>
+				{/* <Navigation> will always render in every path */}
+				<Route path="/" element={<Navigation />}>
+					{/* <Home> will render when root route, at <Navigation>'s <Outlet> */}
+					<Route index element={<Home />} />
+					{/*  	match any route that prefix "/shop/"	*/}
+					<Route path="shop/*" element={<Shop />} />
+					<Route path="auth" element={<Authentication />} />
+					<Route path="checkout" element={<Checkout />} />
+				</Route>
+			</Routes>
+		</Suspense>
 	);
 };
 
